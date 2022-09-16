355ef9beb27515e54698279fa835236f280726d5
Willy <willy.xevo@gmail.com>
2022-09-17 02:55:25 +0700

    ini dia

--- server-local.js
+++ server-local.js
@@ -1,3 +1,3 @@
 const app = require('./src/api');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+app.listen(5000, () => console.log('Local app listening on port 5000!'));

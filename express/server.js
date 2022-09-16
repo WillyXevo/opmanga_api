b4a336c9a649ae7a4bde2a2168860811a3c40fa0
Willy <willy.xevo@gmail.com>
2022-09-17 03:13:17 +0700

    fix ya

---
+++ express/server.js
@@ -0,0 +1,29 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const scp = require('./scp.js');
+
+const router = express.Router();
+router.get('/', async (req, res) => {
+  let url = "https://mangakita.net/manga/one-piece/";
+  console.log(url);
+  let hsl = await scp.minta_semua(url);
+  res.json(hsl);
+});
+router.get('/:link', async (req, res) => {
+  let url = "https://mangakita.net/" + req.params.link;
+  let hsl = await scp.minta_gambar(url);
+  res.json(hsl);
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
--- server-local.js
+++ server-local.js
@@ -1,3 +1,5 @@
-const app = require('./src/api');
+'use strict';
+
+const app = require('./express/server');
 
 app.listen(5000, () => console.log('Local app listening on port 5000!'));

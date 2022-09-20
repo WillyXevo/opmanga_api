14ae7a2f00264d2648229ba89626b2cb8f56299e
Willy <willy.xevo@gmail.com>
2022-09-21 02:12:34 +0700

    change again

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# OPMANGA REST API
+# OPManga
+
+## Get started on Netlify-Express App locally
+
+The most recent information from 2018 confirms that Netlify [does not support sub-folders](https://github.com/netlify/build-image/issues/81) (though there is discussion about adding it to the UI). Therefore all of your project files must exist in the root. It should be noted that while your Netlify routes exist in[`server.js`](https://github.com/king-melchizedek/Netlify-Express/blob/master/express/server.js), your local routes exist as separate files in the [`routes`](https://github.com/king-melchizedek/Netlify-Express/tree/master/routes) subdirectory. Any changes in these files during development must be manually changed in the [`server.js`](https://github.com/king-melchizedek/Netlify-Express/blob/master/express/server.js) file to take effect on Netlify.
+
+To get started, ensure you have [Node.js](https://nodejs.org/en/) installed, then:
+
+``` bash
+npm install
+npm start
+```
+
+### Entry Point
+
+[`server-local.js`](server-local.js) is our entry point. If you have nodemon, run:
+
+``` bash
+nodemon server-local.js
+```
+
+### Pug on Netlify
+
+Pug [isn't supported](https://dev.to/spekulatius1984/how-to-use-pug-on-netlify-3le0) on Netlify. Use pug-cli to compile pug into html.
+
+``` bash
+npm install pug-cli
+node node_modules/pug-cli/index.js pug/ --out ./views
+```
+
+Be sure the build command on Netlify includes the PUG conversion (set in the [`netlify.toml](https://github.com/king-melchizedek/Netlify-Express/blob/master/netlify.toml) file):
+
+``` bash
+npm install && npm run build && node node_modules/pug-cli/index.js views/ --out ./
+```
--- express/server.js
+++ express/server.js
@@ -4,25 +4,47 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const router = express.Router();
 const scp = require('./scp.js');
 
-const router = express.Router();
-router.get('/', async (req, res) => {
+// Link to views folder.
+let views = path.join(__dirname, '../');
+
+// Home route.
+router.get('/', (req, res) => {
+  res.sendFile('index.html', { root: views });
+});
+
+// Other routes.
+router.get('/page1', function(req, res){
+  res.sendFile('page1.html', { root: views });
+});
+router.get('/page2', function(req, res){
+  res.sendFile('page2.html', { root: views });
+});
+router.get('/page3', function(req, res){
+  res.sendFile('page3.html', { root: views });
+});
+router.get('/page4', function(req, res){
+  res.sendFile('page4.html', { root: views });
+});
+
+router.get('/api', async (req, res) => {
   let url = "https://mangakita.net/manga/one-piece/";
   let hsl = await scp.minta_semua(url);
   res.json(hsl);
 });
-router.get('/:link', async (req, res) => {
+
+router.get('/api/:link', async (req, res) => {
   let url = "https://mangakita.net/" + req.params.link;
   let hsl = await scp.minta_gambar(url);
   res.json(hsl);
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
+//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
